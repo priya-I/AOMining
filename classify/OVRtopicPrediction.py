3021d0ab095ed102345233b4d3801231a387783e
priya-I <priya_iyer@berkeley.edu>
2013-05-15 23:49:27 -0700

    Committing the almost final code

---
+++ classify/OVRtopicPrediction.py
@@ -0,0 +1,180 @@
+'''
+Created on May 8, 2013
+
+@author: priya
+'''
+
+from sklearn.multiclass import OneVsRestClassifier as ovr
+from sklearn.tree import DecisionTreeClassifier as dtc
+from sklearn import metrics
+import numpy as np
+import pydot
+import StringIO
+from sklearn import tree
+
+trainfile="data/top4.csv"
+testf="data/testdata.csv"
+villages=[]
+trtgrp=['O','T','TH']
+months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+forums=['Questions and Answers','Announcements','Experience Sharing','Radio']
+crops=[]
+topics=[]
+#forum,crops,topic,dur,villag,age,group,month
+
+'''
+Step:1 Pre-processing
+'''
+def preprocess(trainfile):
+    myMatrix=[]
+    with open(trainfile,"r") as inputf:
+        for row in inputf:
+            record=row.split(",")
+            forum=record[0]
+            crop=record[1]
+            if crop=='':
+                crop='None'
+            topic=record[2]
+            duration=record[3]
+            village=record[4]
+            if(village not in villages):
+                villages.append(village)
+            if(crop not in crops):
+                crops.append(crop)
+            if(topic not in topics):
+                topics.append(topic)
+            age=record[5]
+            
+            month=record[6].strip('\r\n')
+            myMatrix.append([forum,crop,long(duration),village,float(age),month,topic])
+    inputf.close()
+    return myMatrix
+
+'''
+Step.2 Data Preparation
+'''
+def prepare(myMatrix):
+    labels=[]
+    dataset=[]
+    for each in myMatrix:
+        forum=forums.index(each[0])
+        crop=crops.index(each[1])
+        dur=each[2]
+        village=villages.index(each[3])
+        age=each[4]
+        
+        month=months.index(each[5])
+        topic=topics.index(each[6])
+        labels.append(topic)
+        dataset.append([crop,dur,village,age,month])
+    return dataset,labels
+
+
+        
+'''
+Step.3 Training the model
+'''
+trainMat=[]
+training=[]
+trainMat=preprocess(trainfile)
+training=prepare(trainMat)
+labels=training[1]
+#Decision Tree
+#topicClf=dtc(criterion='entropy',random_state=0)
+#topicClf=dtc(random_state=0)
+#topicClf = RFC(n_estimators=12, max_features=5, random_state=0)
+topicClf=ovr(dtc(criterion='entropy',random_state=0))
+topicClf.fit(training[0],labels)
+#print topicClf.multilabel_ 
+#scores = cross_val_score(topicClf, training[0], labels)
+#print "Mean2: ",scores.mean()    
+                            
+'''
+topicClf = dtc(max_depth=None, min_samples_split=1,random_state=0)
+scores = cross_val_score(topicClf, training[0], labels)
+print "Mean1: ", scores.mean() 
+
+                         
+
+topicClf = ETC(n_estimators=10, max_depth=None,min_samples_split=1, random_state=0)
+scores = cross_val_score(topicClf, training[0], labels)
+print "Mean3: ",scores.mean()   
+'''
+#Step.4 Testing the model
+
+
+testMat=[]
+testing=[]
+testData=[]
+testMat=preprocess(testf)
+testing=prepare(testMat)
+testData=testing[0]
+testLabels=testing[1]
+predictions=topicClf.predict(testData)
+
+val=topicClf.score(testData,testLabels)
+
+pestEst=topicClf.estimators_[0]
+disEst=topicClf.estimators_[1]
+fertEst=topicClf.estimators_[2]
+planEst=topicClf.estimators_[3]
+
+pestProba=pestEst.predict_proba(testData)
+print pestProba.shape
+'''
+for (x,y),val in np.ndenumerate(pestProba):
+    print x,y,val
+'''  
+pestPred=pestEst.predict_proba(testData)
+disPred=disEst.predict_proba(testData)
+fertPred=fertEst.predict_proba(testData)
+planPred=planEst.predict_proba(testData)
+pestsornot=['Not','Pests']
+disornot=['Not','Diseases']
+fertornot=['Not','Fertilizers']
+planornot=['Not','Crop Planning']
+pp=[]
+for p in pestPred:
+    #pp.append(pestsornot[p])
+    pp.append(p)
+
+dp=[]
+for p in disPred:
+    #dp.append(disornot[p])
+    dp.append(p)
+
+fp=[]
+for p in fertPred:
+    #fp.append(fertornot[p])
+    fp.append(p)
+
+plp=[]
+for p in planPred:
+    #plp.append(planornot[p])
+    plp.append(p)
+preds=topicClf.predict(testData)
+i=0
+finals=[]
+print pp
+while(i<len(testData)):
+    if((pp[i]-dp[i]-fp[i]-plp[i]).all()==0):
+        finals.append("Other")
+    else:
+        finals.append(topics[preds[i]])
+    i+=1
+for f in finals:
+    print f
+
+print "Mean Accuracy: ",val
+print "Classification Report: \n", metrics.classification_report(testLabels,predictions)
+print topics
+
+'''
+dot_data = StringIO.StringIO() 
+tree.export_graphviz(pestEst, out_file=dot_data) 
+graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
+graph.write_pdf("AO.pdf") 
+
+
+
+'''
--- classify/topicPrediction.py
+++ classify/topicPrediction.py
@@ -5,81 +5,104 @@ Created on May 8, 2013
 '''
 
 from sklearn.tree import DecisionTreeClassifier as dtc
-from sklearn import metrics
-from sklearn import tree
+import cPickle
+from sklearn.metrics import classification_report
+from sklearn import cross_validation
 
+#Input files required for classification
+trainfile="data/TrainingSet.csv"
+testf="data/TestSet.csv"
 
-inputfile="data/top4.csv"
-testf="data/testdata.csv"
-villages=[]
-trtgrp=['O','T','TH']
+#Declaration of variables
 months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-forums=['Questions and Answers','Announcements','Experience Sharing','Radio']
 crops=[]
-topics=[]
-#forum,crops,topic,dur,villag,age,group,month
+topics=['Animal Husbandry', 'Crop Planning', 'Crop Variety', 'Diseases', 'Fertilizers/Bio-organic', 'Government', 'Harvesting', 'Horticulture', 'IPM Strategy', 'Irrigation', 'Land Preparation', 'Marketing', 'NGOs', 'Other', 'Pests', 'Seeds', 'Soil Care', 'Sowing', 'Weather', 'Weed Control']
+testdata=[]
 
 '''
 Step:1 Pre-processing
+The preprocess() function extracts the required fields from the .csv input file which has records in the following format:
+crop, actual topic, call duration, month
+Input: file containing the records
+Output: a list containing the records
 '''
 def preprocess(inputfile):
     myMatrix=[]
+    #messagecrop,messagetopic,listendurations,Month
     with open(inputfile,"r") as inputf:
         for row in inputf:
             record=row.split(",")
-            forum=record[0]
-            crop=record[1]
+            crop=record[0]
             if crop=='':
                 crop='None'
-            topic=record[2]
-            duration=record[3]
-            village=record[4]
-            if(village not in villages):
-                villages.append(village)
+            topic=record[1]
+            duration=record[2]
             if(crop not in crops):
                 crops.append(crop)
             if(topic not in topics):
                 topics.append(topic)
-            age=record[5]
-            trt=record[6]
-            month=record[7].strip('\r\n')
-            myMatrix.append([forum,crop,long(duration),village,float(age),trt,month,topic])
+            month=record[3].strip('\r\n')
+            myMatrix.append([crop,long(duration),month,topic])
     inputf.close()
     return myMatrix
 
 '''
 Step.2 Data Preparation
+The prepare() function processes the list from the preprocess() function to separate the class labels and vectorize the nominal variables such as "crop" and "month" 
+Input: list containing the records
+Output: Two lists containing vectorized training data set and class labels respectively
 '''
 def prepare(myMatrix):
     labels=[]
     dataset=[]
     for each in myMatrix:
-        forum=forums.index(each[0])
-        crop=crops.index(each[1])
-        dur=each[2]
-        village=villages.index(each[3])
-        age=each[4]
-        trt=trtgrp.index(each[5])
-        month=months.index(each[6])
-        topic=topics.index(each[7])
+        crop=crops.index(each[0])
+        dur=each[1]
+        month=months.index(each[2])
+        topic=topics.index(each[3])
         labels.append(topic)
-        dataset.append([forum,crop,dur,village,age,trt,month])
+        dataset.append([crop,dur,month])
     return dataset,labels
-
 
 '''
 Step.3 Training the model
+Processed records are then passed to sklearn's Decision Tree to train the model.
 '''
+
 trainMat=[]
 training=[]
-trainMat=preprocess(inputfile)
+trainMat=preprocess(trainfile)
 training=prepare(trainMat)
 labels=training[1]
-topicClf=dtc(criterion='entropy',random_state=0)
+
+'''
+Fitting the training data into the decision tree
+'''
+topicClf = dtc(criterion='entropy',random_state=0)
 topicClf.fit(training[0],labels)
 
 '''
+Cross validating the results using 10% of the training set as the test set 
+'''
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+training[0], labels, test_size=0.1, random_state=0)
+print "Cross Validation Score"
+print topicClf.score(X_test, y_test)                                        
+
+
+# Save the classifier
+with open('topic_classifier.pkl', 'wb') as fid:
+    cPickle.dump(topicClf, fid)    
+
+'''
+# load the classifier
+with open('topic_classifier.pkl', 'rb') as fid:
+    topicClf = cPickle.load(fid)
+'''
+
+'''
 Step.4 Testing the model
+Passing the test dataset to the classifier 
 '''
 testMat=[]
 testing=[]
@@ -88,12 +111,30 @@ testMat=preprocess(testf)
 testing=prepare(testMat)
 testData=testing[0]
 testLabels=testing[1]
-predictions=topicClf.predict(testData)
-for p in predictions:
-    print topics[p]
+predictions=topicClf.predict_proba(testData)
+predictLabels=topicClf.predict(testData)
 val=topicClf.score(testData,testLabels)
 print "Mean Accuracy: ",val
-print "Classification Report: \n", metrics.classification_report(testLabels,predictions)
-print topics
+print "Classification Report: \n", classification_report(testLabels, predictLabels)
 
 
+'''
+Formatting the output. Check output file: OutputClassifier
+'''
+with open(testf,'r') as testfile:
+    for row in testfile:
+        testdata.append(row)
+print "Test Prediction Results"
+print "The top row is the actual record: crop,actual topic, call duration, month."
+print "The percentage-wise breakdown of the predicted topic are below each of the actual records."
+testResult=[]
+k=0
+for each in predictions:
+    print "Record #"+str(k)+" : "+str(testdata[k])
+    j=0
+    for i in each:
+        if(i!=0):
+            print [topics[j]+": "+str(round(i,3)*100)+"%"]
+        j+=1
+    k+=1
+    print "**************************"
